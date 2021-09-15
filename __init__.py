c8ffaf00f89e7e84af78d828378c8334c10c53de
Tom Kingsford <tkin063@aucklanduni.ac.nz>
2021-09-15 13:43:37 +1200

    refactor: add __init__.py and export various modules

---
+++ __init__.py
@@ -0,0 +1,7 @@
+import scripts.data_loader as data_loader
+import scripts.model as model
+import scripts.train_eval as train_eval
+import scripts.utils as utils
+import scripts.inference as inference
+import scripts.train as train
+import scripts.trinity_data_to_lmdb as trinity_data_to_lmdb
